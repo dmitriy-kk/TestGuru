6b721b1d75aed6c13fc1dd85526739e369fe4bcd
dmitriy-kk <dmitriy-kk@gmail.com>
2024-11-06 19:23:21 +0500

    feat: added entities category, test, question, answer, user

---
+++ db/migrate/20241030123341_add_answers_body_null_constraint.rb
@@ -0,0 +1,5 @@
+class AddAnswersBodyNullConstraint < ActiveRecord::Migration[7.2]
+  def change
+    change_column_null(:answers, :body, false)
+  end
+end
---
+++ db/migrate/20241106140310_remove_correct_to_answers.rb
@@ -0,0 +1,5 @@
+class RemoveCorrectToAnswers < ActiveRecord::Migration[7.2]
+  def change
+    remove_column :answers, :correct
+  end
+end
